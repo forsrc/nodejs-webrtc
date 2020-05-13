0e5bd14191221effee9c0c09cff69be78fef9698
forsrc <forsrc@gmail.com>
2020-05-13 19:01:59 +0900

    use node-turn

--- index.js
+++ index.js
@@ -1,14 +1,65 @@
-require('dotenv').config();
-
-// Twilio init
-var twilio = require('twilio')(
-  process.env.TWILIO_ACCOUNT_SID,
-  process.env.TWILIO_AUTH_TOKEN
-);
 var express = require('express');
 var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+
+const stun = require('stun');
+const { STUN_BINDING_RESPONSE, STUN_EVENT_BINDING_REQUEST } = stun.constants;
+const Turn = require('node-turn');
+
+
+var turnServer = {}
+turnServer.run = function() {
+    var turnPort = process.env.TURN_PORT || 3478;
+    var server = new Turn({
+        listeningPort: turnPort,
+        listeningIps: process.env.TURN_IPS ? process.env.TURN_IPS.split(",") : [process.env.TURN_IP || '0.0.0.0'],
+        authMech: 'long-term',
+        credentials: {
+            forsrc: "forsrc"
+        }
+    });
+    server.start();
+    console.log('[turn] server started port:', turnPort, server);
+    this.handle(server);
+};
+turnServer.handle = function(server) {
+}
+turnServer.run();
+
+var stunServer = {}
+
+stunServer.run = function() {
+    const server = stun.createServer({ type: 'udp4' });
+    const port = process.env.STUN_PORT || 19302;
+    server.listen(port, "0.0.0.0", () => {
+        console.log('[stun] server started port:' + port);
+    });
+
+    stunServer.handle(server);
+    stun.request('localhost:' + port, (err, res) => {
+        if (err) {
+            console.error(err);
+        } else {
+            const { address } = res.getXorAddress();
+            console.log('stun ip', address);
+      }
+    });
+
+};
+
+stunServer.handle = function(server) {
+    const userAgent = `node/${process.version} stun/v1.0.0`;
+    server.on(STUN_EVENT_BINDING_REQUEST, (request, rinfo) => {
+        const message = stun.createMessage(STUN_BINDING_RESPONSE, request.transactionId);
+        message.addXorAddress(rinfo.address, rinfo.port);
+        message.addSoftware(userAgent);
+        server.send(message, rinfo.port, rinfo.address);
+    });
+}
+
+//stunServer.run();
+
 
 app.use(express.static('public'));
 // When a socket connects, set up the specific listeners we will use.
@@ -35,16 +86,11 @@ io.on('connection', function(socket){
   // When receiving the token message, use the Twilio REST API to request an
   // token to get ephemeral credentials to use the TURN server.
   socket.on('token', function(){
-    console.log('Received token request')
-    twilio.tokens.create(function(err, response){
-      if(err){
-        console.log(err);
-      }else{
-        // Return the token to the browser.
-        console.log('Token generated. Returning it to the client')
-        socket.emit('token', response);
-      }
-    });
+	var ip = process.env.TURN_IP || '0.0.0.0';
+	var token = {"iceServers":[{"urls":[`turn:${ip}:3478`],"username":"forsrc","credential":"forsrc"}],"iceTransportPolicy":"all","iceCandidatePoolSize":"0"};
+	console.log('Received token request', token)
+    socket.emit('token', token);
+
   });
 
   // Relay candidate messages

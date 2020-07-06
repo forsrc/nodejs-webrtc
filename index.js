ee2a513066fe7227bd4a68b31cdf921fe8889f37
forsrc <forsrc@192.168.2.102>
2020-07-06 23:48:41 +0900

    webrtc multi peer

--- index.js
+++ index.js
@@ -110,5 +110,21 @@ io.on('connection', function (socket) {
     socket.to(room).emit('answer', answer);
     //socket.broadcast.emit('answer', answer);
   });
+
+  ///////////////
+
+  io.sockets.emit("user-joined", socket.id, io.engine.clientsCount, Object.keys(io.sockets.clients().sockets));
+
+  socket.on('signal', (toId, message) => {
+    io.to(toId).emit('signal', socket.id, message);
+  });
+
+  socket.on("message", function (data) {
+    io.sockets.emit("broadcast-message", socket.id, data);
+  })
+
+  socket.on('disconnect', function () {
+    io.sockets.emit("user-left", socket.id);
+  })
 });
 
